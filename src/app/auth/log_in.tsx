db5d6146146630b3a70a0ec661f3c6be78cc6858
Hop-Step-Jump <122687682+Hop-Step-Jump@users.noreply.github.com>
2025-03-26 08:54:52 +0900

    Sec12-82

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -2,21 +2,18 @@ import {
   View, Text, TextInput,
   TouchableOpacity, StyleSheet
 } from 'react-native'
-
 import { Link, router } from 'expo-router'
 
-import Header from '../../components/Header'
 import Button from '../../components/Button'
 
 const handlePress = (): void => {
   // ログイン
-  router.push('/memo/list')
+  router.replace('/memo/list')
 }
 
 const LogIn = (): JSX.Element => {
   return (
     <View style={styles.container}>
-      <Header />
       <View style={styles.inner}>
         <Text style={styles.title}>Log In</Text>
         <TextInput style={styles.input} value='Email address' />
