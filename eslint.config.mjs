f5442866dcb0b9c7b8a729f06eb64383497061d8
Hop-Step-Jump <122687682+Hop-Step-Jump@users.noreply.github.com>
2025-03-31 10:49:49 +0900

    Sec14-108

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js"
 
 export default [
